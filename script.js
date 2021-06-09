2d7a539ed1146939543ffcdd8076cb0dc7769937
bolattt <bolattny@gmail.com>
2021-06-09 14:46:31 -0400

    populate ui with data from local storage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modalClose = document.getElementById("close-modal");
 const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
-const bookmarksConatiner = document.getElementById("bookmarks-container");
+const bookmarksContainer = document.getElementById("bookmarks-container");
 
 let bookmarks = [];
 
@@ -32,18 +32,52 @@ function validate(nameValue, urlValue) {
     alert("Please submit values for both fields.");
     return false;
   }
-  if (urlValue.match(regex)) {
-    alert("match");
-  }
+  //   if (urlValue.match(regex)) {
+  //     alert("match");
+  //   }
   if (!urlValue.match(regex)) {
     alert("Please provide a valid URL");
     return false;
   }
   console.log(nameValue, urlValue);
-  console.log("validate was called");
 
   // Valid
   return true;
+}
+
+// Build bookmarks DOM
+function buildBookmarks() {
+  // Build Items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    // Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      "https://github.com/bolattt/joke-teller/blob/main/fav.png?raw=true"
+    );
+    favicon.setAttribute("alt", "Favicon");
+    // Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
 }
 
 // Fetch Bookmarks
@@ -54,14 +88,15 @@ function fetchBookmarks() {
   } else {
     // Create bookmarks array in localStorage
     bookmarks = [
-        {
-            name: "My Github";
-            url: "https://github.com/bolattt";
-        }
+      {
+        name: "My Github",
+        url: "https://github.com/bolattt",
+      },
     ];
 
-    localStorage.setItem('bookmarks',JSON.stringify(bookmarks))
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
+  buildBookmarks();
 }
 
 // Handle Data from Form
@@ -91,5 +126,5 @@ function storeBookmark(e) {
 // Event Listeners
 bookmarkForm.addEventListener("submit", storeBookmark);
 
-// On Load 
+// On Load
 fetchBookmarks();
